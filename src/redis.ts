f90b75bef17ee3c42edeb83a248886f6946f2963
Mohammad Arkan <mdarkanurl@gmail.com>
2026-03-30 20:52:31 +0600

    Added listener to get redis connected time

--- src/redis.ts
+++ src/redis.ts
@@ -7,3 +7,15 @@ const redisUrl =
   configService.get<string>("REDIS_URL") || "redis://localhost:6379";
 
 export const redis = new Redis(redisUrl);
+
+let redisConnectedAt: Date | null = null;
+
+redis.on("ready", () => {
+  redisConnectedAt = new Date();
+});
+
+redis.on("close", () => {
+  redisConnectedAt = null;
+});
+
+export const getRedisConnectedAt = () => redisConnectedAt;
