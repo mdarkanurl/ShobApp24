d29843b5170f8d4662dfce6f79836b0747a39613
Mohammad Arkan <mdarkanurl@gmail.com>
2026-04-10 23:28:06 +0600

    Star event almost done

--- src/worker/github/core/event/Star/star-event.ts
+++ src/worker/github/core/event/Star/star-event.ts
@@ -3,6 +3,9 @@ import { ConfigService } from "@nestjs/config";
 import { githubStarEventSchemaDto } from "./dto/github-star-webhook.dto";
 import { Class_methods_type } from "../../../types/class-methods-type";
 import { PrismaService } from "../../../../../prisma/prisma.service";
+import { sendEmail } from '../../../../../utils/rabbitmq';
+import { createActionSchema } from "../../../../../action/dto/create-action.dto";
+import { collect_viewer_info } from "./actions/collect_viewer_info";
 
 
 export class Star_event {
@@ -12,10 +15,10 @@ export class Star_event {
     }
 
     async Star_event(
-        data: githubStarEventSchemaDto
+        dataset: githubStarEventSchemaDto
     ): Promise<Class_methods_type> {
         try {
-            const payload = data.data;
+            const payload = dataset.data;
             
             if(payload.action === "deleted") {
                 /**
@@ -99,6 +102,106 @@ export class Star_event {
             }
 
             // TODO write logic for execute actions
+            for (let i = 0; i < actions.length; i++) {
+                const { success, data, error } = createActionSchema.safeParse(actions[i]);
+
+                if(!success) {
+                    return {
+                        success: false,
+                        message: error.message,
+                        allUpTo: false,
+                        requeue: false
+                    }
+                }
+
+                const actionType = data.type;
+                if(actionType === "send_email" && data.config.do_you_wanto_to_send_viewer_info) {
+                    // Collect viewer info
+                    const userData = await collect_viewer_info(dataset);
+
+                    // Send email
+                    await sendEmail({
+                        email: data.config.email,
+                        subject: data.config.subject,
+                        body: `${data.config.body}\n\nHere's the viewer info:\n${JSON.stringify(userData)}`
+                    });
+
+                    return {
+                        success: true
+                    };
+                } else if(actionType === "send_email") {
+                    await sendEmail({
+                        email: data.config.email,
+                        subject: data.config.subject,
+                        body: data.config.body
+                    });
+
+                    return {
+                        success: true
+                    };
+                }
+
+                if(actionType === "send_email_to_me" && data.config.do_you_want_viewer_info) {
+                    // Collect viewer info
+                    const userData = await collect_viewer_info(dataset);
+
+                    // Send email
+                    await sendEmail({
+                        email: data.config.email,
+                        subject: data.config.subject || "",
+                        body: `${data.config.body}\n\nHere's the viewer info:\n${JSON.stringify(userData)}`
+                    });
+
+                    return {
+                        success: true
+                    };
+                } else if(actionType === "send_email_to_me") {
+                    await sendEmail({
+                        email: data.config.email,
+                        subject: data.config.subject || "",
+                        body: data.config.body || ""
+                    });
+
+                    return {
+                        success: true
+                    };
+                }
+
+                if(actionType === "send_email_to_who_send_the_trigger") {
+                    const userData = await collect_viewer_info(dataset);
+
+                    await sendEmail({
+                        email: userData.email,
+                        subject: data.config.subject || "",
+                        body: data.config.body
+                    });
+                    
+                    return {
+                        success: true
+                    };
+                }
+
+                if(actionType === "webhook") {
+                    const userData = await collect_viewer_info(dataset);
+
+                    await fetch(data.config.url, {
+                        method: 'POST',
+                        headers: {
+                            'User-Agent': 'ShobApp24-webhook',
+                            'Content-Type': 'application/json',
+                        },
+                        body: JSON.stringify(userData),
+                    });
+
+                    return {
+                        success: true
+                    };
+                }
+
+                if(actionType === "send_telegram") {
+                    // TODO send telegram message
+                }
+            }
 
             return {
                 success: true
