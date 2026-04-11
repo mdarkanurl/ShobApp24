c8b7dfa335f423c28a5905f951fd5ee264720184
Mohammad Arkan <mdarkanurl@gmail.com>
2026-04-11 17:48:16 +0600

    Updated workflow schema

--- src/Workflow/dto/create-workflow.dto.ts
+++ src/Workflow/dto/create-workflow.dto.ts
@@ -1,12 +1,21 @@
-import { Platform } from "@prisma/client";
+import { EventType, Platform, Prisma } from "@prisma/client";
 import { z } from "zod";
 
 export const createWorkflowSchema = z.object({
     name: z.string().min(1),
     platform: z.nativeEnum(Platform),
 
+    eventType: z.nativeEnum(EventType),
+    action: z.string().min(1).optional(),
+      config: z.json().nullable().optional().transform((val) => {
+        if (val === null) return Prisma.JsonNull;
+        return val;
+    }),
+
     resourceId: z.string().optional(),
     resourceType: z.string().optional()
+}).refine((data) => data.platform === "GitHub" && data.action, {
+    message: "You must specific the action"
 });
 
 export type createWorkflowSchemaDto = z.infer<typeof createWorkflowSchema>;
