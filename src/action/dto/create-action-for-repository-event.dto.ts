0a9ce8ebcff4d595982d49d08de6eb230145b99a
Mohammad Arkan <mdarkanurl@gmail.com>
2026-04-21 12:50:44 +0600

    Added repo event schema

---
+++ src/action/dto/create-action-for-repository-event.dto.ts
@@ -0,0 +1,19 @@
+import { ActionTypes, Platform } from "@prisma/client";
+import { z } from "zod";
+
+// helper function
+const sendEmailRepoEventConfigSchema = z.object({
+    email: z.string().trim().toLowerCase().email(),
+    subject: z.string().trim().min(3).max(900).optional(),
+    body: z.string().trim().min(3).max(10000).optional(),
+    do_you_want_repo_info: z.boolean().default(false)
+});
+
+// repo event schema
+export const sendEmailRepoEventSchema = z.object({
+    platform: z.nativeEnum(Platform),
+    type: z.literal(ActionTypes.send_email),
+    config: sendEmailRepoEventConfigSchema,
+    step: z.number(),
+});
+
