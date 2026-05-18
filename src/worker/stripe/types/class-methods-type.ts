19555f0ae39f304fc0d5a19fcd9f0c1e12ab328e
Mohammad Arkan <mdarkanurl@gmail.com>
2026-05-18 10:05:43 +0600

    Added type for class methods

---
+++ src/worker/stripe/types/class-methods-type.ts
@@ -0,0 +1,10 @@
+export type Class_methods_type = {
+    success: false,
+    message: string,
+    allUpTo: boolean,
+    requeue: boolean
+} | {
+    success: true,
+    message?: string,
+    allUpTo?: boolean
+}
