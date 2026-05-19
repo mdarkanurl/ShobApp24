8e46d2501f085b9a592c200e05b9cdfb4bbc9d99
Mohammad Arkan <mdarkanurl@gmail.com>
2026-05-19 21:29:19 +0600

    Invoice paid event done

--- src/worker/stripe/core/event/Invoice_paid/invoice-paid-event.ts
+++ src/worker/stripe/core/event/Invoice_paid/invoice-paid-event.ts
@@ -1,12 +1,44 @@
+import { PrismaClient } from "@prisma/client";
 import { Class_methods_type } from "../../../types/class-methods-type";
+import { PrismaService } from "../../../../../prisma/prisma.service";
+import { ConfigService } from "@nestjs/config";
+import { InvoicePaidEvent } from "./dto/invoice_paid.dto";
 
 export class Invoice_paid_event {
+    protected readonly prisma: PrismaClient;
+    
+    constructor(prisma?: PrismaClient) {
+        this.prisma = prisma ?? new PrismaService(new ConfigService());
+    }
+
     async Invoice_paid_event(
         payload: any
     ): Promise<Class_methods_type> {
+        const data = payload.data as InvoicePaidEvent;
         try {
-            console.log("Processing invoice.paid event");
-            // TODO: Implement logic
+            await this.prisma.subscriptions.upsert({
+                create: {
+                    stripeSubscriptionId: data.data.object.parent.subscription_details.subscription,
+                    currentPeriodStart: data.data.object.period_start.toString(),
+                    currentPeriodEnd: data.data.object.period_start.toString(),
+                    status: "active"
+                },
+                update: {
+                    currentPeriodStart: data.data.object.period_start.toString(),
+                    currentPeriodEnd: data.data.object.period_start.toString(),
+                },
+                where: {
+                    stripeSubscriptionId: data.data.object.parent.subscription_details.subscription
+                }
+            });
+
+            
+            //  TODO send confirmation email
+            // sendEmail({
+            //     email: payload.data.data.object.customer_details.email,
+            //     subject: `You have successfully subscribe to ${payload.data.data.object.metadata.plan} plan`,
+            //     body: `You have successfully subscribe to ${payload.data.data.object.metadata.plan} plan`,
+            // });
             return {
                 success: true
             };
