eec6aa289bc8666304f4870c7e91f35f5ea9eb63
Mohammad Arkan <mdarkanurl@gmail.com>
2026-05-19 22:20:17 +0600

    invoice payment failed event done

--- src/worker/stripe/core/event/Invoice_payment_failed/invoice-payment-failed-event.ts
+++ src/worker/stripe/core/event/Invoice_payment_failed/invoice-payment-failed-event.ts
@@ -1,12 +1,50 @@
+import { PrismaClient } from "@prisma/client";
 import { Class_methods_type } from "../../../types/class-methods-type";
+import { PrismaService } from "../../../../../prisma/prisma.service";
+import { ConfigService } from "@nestjs/config";
 
 export class Invoice_payment_failed_event {
+    protected readonly prisma: PrismaClient;
+        
+    constructor(prisma?: PrismaClient) {
+        this.prisma = prisma ?? new PrismaService(new ConfigService());
+    }
+
     async Invoice_payment_failed_event(
         payload: any
     ): Promise<Class_methods_type> {
+        const data = payload.data;
         try {
-            console.log("Processing invoice.payment_failed event");
-            // TODO: Implement logic
+            await this.prisma.subscriptions.upsert({
+                create: {
+                    stripeSubscriptionId: data.data.object.parent.subscription_details.subscription,
+                    currentPeriodStart: data.data.object.period_start.toString(),
+                    currentPeriodEnd: data.data.object.period_start.toString(),
+                    status: "unpaid"
+                },
+                update: {
+                    currentPeriodStart: data.data.object.period_start.toString(),
+                    currentPeriodEnd: data.data.object.period_start.toString(),
+                    atatus: "unpaid"
+                },
+                where: {
+                    stripeSubscriptionId: data.data.object.parent.subscription_details.subscription
+                }
+            });
+
+            // insert data to payments table
+            await this.prisma.payments.create({
+                data: {
+                    stripeInvoiceId: data.data.object.id,
+                    stripeSubscriptionId: data.data.object.parent.subscription_details.subscription,
+                    amount: data.data.object.amount_paid,
+                    currency: data.data.object.currency,
+                    status: "payment_failed",
+                    paidAt: data.data.object.created.toString()
+                }
+            });
+            
+            //  TODO send confirmation email
             return {
                 success: true
             };
