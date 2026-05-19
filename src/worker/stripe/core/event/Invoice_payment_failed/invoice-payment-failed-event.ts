06c1e5832e23b6c830ac12cb707ed9d40f7594c8
Mohammad Arkan <mdarkanurl@gmail.com>
2026-05-19 23:10:44 +0600

    Fixed typo

--- src/worker/stripe/core/event/Invoice_payment_failed/invoice-payment-failed-event.ts
+++ src/worker/stripe/core/event/Invoice_payment_failed/invoice-payment-failed-event.ts
@@ -25,7 +25,7 @@ export class Invoice_payment_failed_event {
                 update: {
                     currentPeriodStart: data.data.object.period_start.toString(),
                     currentPeriodEnd: data.data.object.period_start.toString(),
-                    atatus: "unpaid"
+                    status: "unpaid"
                 },
                 where: {
                     stripeSubscriptionId: data.data.object.parent.subscription_details.subscription
